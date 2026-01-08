d76ec292417acaa534d719a48c025de46f5d0b55
İsmail Durcan <ismaildurcan40@gmail.com>
2026-01-08 21:55:15 +0300

    Add Cuisine entity with attributes and JPA annotations

---
+++ src/main/java/com/ismaildrcn/model/entity/Cuisine.java
@@ -0,0 +1,34 @@
+package com.ismaildrcn.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "cuisines")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(callSuper = true)
+public class Cuisine extends BaseEntity {
+
+    private String name;
+
+    private String slug;
+
+    private String description;
+
+    @Column(name = "icon_url")
+    private String iconUrl;
+
+    @Column(name = "sort_order")
+    private Integer sortOrder = 0;
+
+    @Column(name = "is_active")
+    private boolean isActive = true;
+
+}
