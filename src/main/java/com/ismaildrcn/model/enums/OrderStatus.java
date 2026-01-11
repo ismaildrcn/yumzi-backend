55e967c335eae64fd6b69eb5b0236e0935b540f3
İsmail Durcan <ismaildurcan40@gmail.com>
2026-01-11 19:35:10 +0300

    Change enum classes

--- src/main/java/com/ismaildrcn/model/enums/OrderStatus.java
+++ src/main/java/com/ismaildrcn/model/enums/OrderStatus.java
@@ -1,16 +1,25 @@
 package com.ismaildrcn.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum OrderStatus {
 
-    PENDING,
-    CONFIRMED,
-    PREPARING,
-    READY_FOR_DELIVERY,
-    DRIVER_ASSIGNED,
-    DRIVER_EN_ROUTE_TO_RESTAURANT,
-    ON_THE_WAY,
-    DELIVERED,
-    COMPLETED,
-    CANCELLED,
-    REFUNDED
+    PENDING("PENDING"),
+    CONFIRMED("CONFIRMED"),
+    PREPARING("PREPARING"),
+    READY_FOR_DELIVERY("READY_FOR_DELIVERY"),
+    DRIVER_ASSIGNED("DRIVER_ASSIGNED"),
+    DRIVER_EN_ROUTE_TO_RESTAURANT("DRIVER_EN_ROUTE_TO_RESTAURANT"),
+    ON_THE_WAY("ON_THE_WAY"),
+    DELIVERED("DELIVERED"),
+    COMPLETED("COMPLETED"),
+    CANCELLED("CANCELLED"),
+    REFUNDED("REFUNDED");
+
+    private final String status;
+
+    OrderStatus(String status) {
+        this.status = status;
+    }
 }
--- src/main/java/com/ismaildrcn/model/enums/PaymentStatus.java
+++ src/main/java/com/ismaildrcn/model/enums/PaymentStatus.java
@@ -1,33 +1,41 @@
 package com.ismaildrcn.model.enums;
 
+import lombok.Getter;
+
+@Getter
 public enum PaymentStatus {
     // Öncesi
-    PENDING, // Beklemede
-    AUTHORIZED, // Yetkilendirildi
+    PENDING("PENDING"), // Beklemede
+    AUTHORIZED("AUTHORIZED"), // Yetkilendirildi
 
     // Başarılı
-    SUCCESSFUL,
-    COMPLETED,
-    CAPTURED, // Tutar alındı
+    SUCCESSFUL("SUCCESSFUL"),
+    COMPLETED("COMPLETED"),
+    CAPTURED("CAPTURED"), // Tutar alındı
 
     // Beklemede
-    PROCESSING,
-    AWAITING_CONFIRMATION,
+    PROCESSING("PROCESSING"),
+    AWAITING_CONFIRMATION("AWAITING_CONFIRMATION"),
 
     // Başarısız
-    FAILED,
-    DECLINED, // Reddedildi
-    EXPIRED, // Süresi doldu
+    FAILED("FAILED"),
+    DECLINED("DECLINED"), // Reddedildi
+    EXPIRED("EXPIRED"), // Süresi doldu
 
     // İade/İptal
-    REFUNDED, // İade edildi
-    PARTIALLY_REFUNDED, // Kısmen iade
-    CANCELLED,
+    REFUNDED("REFUNDED"), // İade edildi
+    PARTIALLY_REFUNDED("PARTIALLY_REFUNDED"), // Kısmen iade
+    CANCELLED("CANCELLED"),
 
     // Riskli
-    UNDER_REVIEW, // İnceleniyor
-    FRAUD_SUSPECTED,
-
+    UNDER_REVIEW("UNDER_REVIEW"), // İnceleniyor
+    FRAUD_SUSPECTED("FRAUD_SUSPECTED"),
     // Beklemede (kapıda ödeme için)
-    AWAITING_COD_PAYMENT
+    AWAITING_COD_PAYMENT("AWAITING_COD_PAYMENT");
+
+    private final String status;
+
+    PaymentStatus(String status) {
+        this.status = status;
+    }
 }
