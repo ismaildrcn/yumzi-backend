f17b44dfff7c1247be43a4bd5a28fe47535e58ac
İsmail Durcan <ismaildurcan40@gmail.com>
2026-01-12 22:02:43 +0300

    Rename dto

--- src/main/java/com/ismaildrcn/model/dto/DtoAddressRequest.java
+++ src/main/java/com/ismaildrcn/model/dto/DtoAddressRequest.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AddressRequest {
+public class DtoAddressRequest {
 
     @NotNull(message = "Title cannot be null")
     private String title;
--- src/main/java/com/ismaildrcn/model/dto/DtoAddressResponse.java
+++ src/main/java/com/ismaildrcn/model/dto/DtoAddressResponse.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class AddressResponse {
+public class DtoAddressResponse {
 
     private Long id;
 
